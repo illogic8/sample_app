c495e3d205d7fb4215538a76913a112d48e1f2ff
Eric <eswim007@yahoo.com>
2013-03-09 12:44:28 -0500

    Postgres Setup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +11,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do 
       visit '/static_pages/home'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "#{base_title}| Home")
     end
   end
 
@@ -23,7 +24,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+        :text => "#{base_title}| Help")
     end
 
   end
@@ -33,11 +34,24 @@ describe "StaticPages" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
-  end
     it "should have the title 'About'" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | About")
+        :text => "#{base_title} | About")
     end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+        :text => "#{base_title}| Contact")
+    end
+  end
+
 
 end
