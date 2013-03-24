d85d47847592dc28d9328d06b305f12f20128e79
Eric <eswim007@yahoo.com>
2013-03-24 10:34:43 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,36 +7,34 @@ describe "Authentication" do
   	before { visit signin_path }
 
   	it { should have_selector('h1', text: 'Sign in') }
-  	it { should have_selector('title', text: 'Sign in') }
+  	it { should have_title('Sign in') }
   end 
 
   describe "signin" do
-  	before { visit signin_path}
+  	before { visit signin_path }
 
     describe "with invalid information" do
     	before { click_button "Sign in" }
      
-        it { should have_selector('title', text: 'Sign in') }
-  	    it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+        it { should have_title('Sign in') }
+  	    it { should have_error_message('Invalid') }
 
   	    describe "after visiting another page" do
   	    	before { click_link "Home" }
-  	    	it { should_not have_selector('div.alert.alert-error') }
+  	    	it { should_not have_error_message }
   	    end
   	end
 
     describe "with valid information" do
-        let(:user) { FactoryGirl.create(:user) }
-    	before do 
-    		fill_in "Email", with: user.email.upcase
-    		fill_in "Password", with: user.password
-    		click_button "Sign in" 
-    	end
+      create_user
+    	before { valid_signin user }
 
-    	it { should have_selector('title', text: user.name) }
+    	it { should have_title(user.name) }
     	it { should have_link('Profile', href: user_path(user)) }
     	it { should have_link('Sign out', href: signout_path) }
     	it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Users', href: users_path) }
 
       describe "followed by signout" do
         before { click_link "Sign out" }
@@ -44,6 +42,78 @@ describe "Authentication" do
         it { should have_link('Sign in') }
       end
     end
-
   end
+
+  describe "authorization" do
+
+    describe "for non-signed in users" do
+      create_user
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          valid_signin(user)
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_title("Edit user")
+          end
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title("Sign in") }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      create_user
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before do
+        visit signin_path
+        valid_signin(user) 
+      end
+
+      describe "visiting Users#edit page" do
+        before {visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      create_user
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before do
+        visit signin_path
+        valid_signin non_admin
+      end
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+  end
+
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,49 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+def valid_signin(user)
+	fill_in "Email", with: user.email.upcase
+    fill_in "Password", with: user.password
+    click_button "Sign in" 
+# Sign in when not using Capybara as well.
+    cookies[:remember_token] = user.remember_token
+end
+
+def valid_signup
+	fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :have_title do |message|
+	match do |page|
+		page.should have_selector('title', text: message)
+	end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-success', text: message)
+	end
+end
+
+def signup_error_messages
+	it { should have_title('Sign up') }
+    it { should have_content('error') }
+    it { should have_content('Name can\'t be blank') }
+    it { should have_content('Email can\'t be blank') }
+    it { should have_content('Email is invalid') }
+    it { should have_content('Password can\'t be blank') }
+    it { should have_content('Password is too short') }
+    it { should have_content('Password confirmation can\'t be blank') }
+end
+
+def create_user
+    let(:user) { FactoryGirl.create(:user) }
+end
